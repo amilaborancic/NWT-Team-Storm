1e84493ec3983b7c378a9a5d10a4127a43900be5
Ahmed Serdarevic <aserdarevi1@etf.unsa.ba>
2020-03-27 10:09:26 +0100

    They try to copy my swagger I'm on that next ....... dodan swagger

--- user-microservice/src/main/java/user/usermicroservice/UserMicroserviceApplication.java
+++ user-microservice/src/main/java/user/usermicroservice/UserMicroserviceApplication.java
@@ -3,9 +3,11 @@ package user.usermicroservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @EnableAutoConfiguration
 @SpringBootApplication
+@EnableSwagger2
 public class UserMicroserviceApplication {
 
 	public static void main(String[] args) {
