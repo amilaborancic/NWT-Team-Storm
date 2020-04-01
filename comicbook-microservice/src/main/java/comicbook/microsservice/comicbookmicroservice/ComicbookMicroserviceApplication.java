1b728abc549dfd49bf64c6754b73b20a2b94f086
Amila <aborancic1@etf.unsa.ba>
2020-04-01 15:36:04 +0200

    Comicbook servis je sada registrovan na eureku

--- comicbook-microservice/src/main/java/comicbook/microsservice/comicbookmicroservice/ComicbookMicroserviceApplication.java
+++ comicbook-microservice/src/main/java/comicbook/microsservice/comicbookmicroservice/ComicbookMicroserviceApplication.java
@@ -12,6 +12,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.stereotype.Component;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
@@ -22,6 +23,7 @@ import java.util.List;
 @EnableJpaRepositories(basePackages = "comicbook.microsservice.comicbookmicroservice.repository")
 @SpringBootApplication
 @EnableSwagger2
+@EnableEurekaClient
 public class ComicbookMicroserviceApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ComicbookMicroserviceApplication.class, args);
