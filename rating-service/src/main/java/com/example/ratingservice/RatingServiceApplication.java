30ba1db5d60901c237245c11416678b23a860b31
Mahira B <mbuturovic1@etf.unsa.ba>
2020-04-02 18:45:49 +0200

    rating servis registrovan na eureku

--- rating-service/src/main/java/com/example/ratingservice/RatingServiceApplication.java
+++ rating-service/src/main/java/com/example/ratingservice/RatingServiceApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.stereotype.Component;
 
@@ -19,6 +20,8 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @EnableSwagger2
 @SpringBootApplication
 @EnableAutoConfiguration
+@EnableEurekaClient
+
 public class RatingServiceApplication {
 
 	public static void main(String[] args) {
