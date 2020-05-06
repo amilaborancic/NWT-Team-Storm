f301fc6a75a131ac9bd3e10223a62f1f7d3a6832
Amila <aborancic1@etf.unsa.ba>
2020-05-06 10:26:47 +0200

    Konfigurisan api gateway

--- user-microservice/src/main/java/user/usermicroservice/UserMicroserviceApplication.java
+++ user-microservice/src/main/java/user/usermicroservice/UserMicroserviceApplication.java
@@ -3,7 +3,6 @@ package user.usermicroservice;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
