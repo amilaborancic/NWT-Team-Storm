fa3bbf7889a47d05c745c8a2add055ce05ed4fa3
Mahira B <mbuturovic1@etf.unsa.ba>
2020-05-07 18:49:05 +0200

    permisije za usera

--- api-gateway/src/main/java/com/example/demo/config/SecurityConfig.java
+++ api-gateway/src/main/java/com/example/demo/config/SecurityConfig.java
@@ -4,7 +4,9 @@ import com.example.demo.service.CustomUserDetailsService;
 import com.netflix.loadbalancer.NoOpPing;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -20,6 +22,20 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         auth.userDetailsService(customUserDetailsService);
+    }
+
+    @Override
+    public void configure(HttpSecurity http) throws Exception{
+        http.httpBasic().and()
+                .authorizeRequests()
+                .antMatchers(HttpMethod.POST,"/user/sign-in", "/user/sign-up").permitAll()
+                .antMatchers(HttpMethod.POST,"/rating/dodaj-rating","/strip/noviStrip","/strip/sviPoId","/katalog/novi").hasRole("USER")
+                .antMatchers(HttpMethod.GET,"/rating/**", "/strip/svi","/strip","/strip/trazi-autor","/strip/trazi-zanr",
+                        "/strip/trazi-izdavac","/strip/trazi-naziv","/katalog/**","/zanr/svi","/izdavac/svi","/autor/svi").hasRole("USER")
+                .antMatchers(HttpMethod.DELETE,"/katalog/**").hasRole("USER")
+                .antMatchers(HttpMethod.PUT,"/katalog/dodavanje-stripa").hasRole("USER")
+                .and()
+                .csrf().disable();
     }
 
     @Bean
