394c065573035e9b71bc4865e10628cbf762bf77
Ahmed Serdarevic <aserdarevi1@etf.unsa.ba>
2020-05-08 06:11:26 +0200

    Dodana komunikacija user servisa sa grpc serverom.

--- user-microservice/src/main/java/user/usermicroservice/Servisi/UserServis.java
+++ user-microservice/src/main/java/user/usermicroservice/Servisi/UserServis.java
@@ -7,6 +7,9 @@ import user.usermicroservice.DTO.UserRatingDTO;
 import user.usermicroservice.Models.User;
 import user.usermicroservice.Repository.UserRepository;
 import user.usermicroservice.exception.ApiRequestException;
+import user.usermicroservice.grpc.EventSubmission;
+import user.usermicroservice.grpc.Events;
+import user.usermicroservice.grpc.actionGrpc;
 
 import java.util.List;
 import java.util.Optional;
@@ -17,20 +20,30 @@ public class UserServis {
 	@Autowired
 	UserRepository userRepository;
 
+	@Autowired
+	EventSubmission eventSubmission;
+
+	private Long idAdmin = 1000L;
+	private Long idLogovanogKorisnika = 500L;
+
 	public Optional<User> findUserById(Long id) {
 		Optional<User> user = userRepository.findById(id);
 		if (user.isEmpty())
 			throw new ApiRequestException("User sa id-jem " + id + " ne postoji!");
+		eventSubmission.addEvent(Events.ActionType.GET, "Korisnik po ID-u:"+ id.toString());
 		return user;
 	}
 
 	public void addNewUser(User user) {
+
+		eventSubmission.addEvent(Events.ActionType.CREATE, "Dodaj novog korisnika.");
 		userRepository.save(user);
 	}
 
 	public User findUserByUserName(String name) {
 		User user = userRepository.findByUserName(name);
 		if(user == null) throw new ApiRequestException("Korisnik sa username-om " + name + " ne postoji!");
+		eventSubmission.addEvent(Events.ActionType.GET, "Korisnik po user-name.");
 		return user;
 	}
 
@@ -52,6 +65,12 @@ public class UserServis {
 			User korisnik_iz_baze = userRepository.getOne(userRatingInfo.getId());
 			korisnik_iz_baze.setBroj_losih_reviewa(userRatingInfo.getBroj_losih_reviewa());
 			korisnik_iz_baze.setUkupno_reviewa(userRatingInfo.getUkupno_reviewa());
+
+			String allRaitings = String.valueOf(userRatingInfo.getUkupno_reviewa());
+			String negativeRaitings = String.valueOf(userRatingInfo.getBroj_losih_reviewa());
+
+			eventSubmission.addEvent(Events.ActionType.UPDATE, "Korisnik ima ukupno reviewa: " +
+					allRaitings +", a od toga je negativnih reviewa: " + negativeRaitings);
 			userRepository.save(korisnik_iz_baze);
 		}
 		else throw new ApiRequestException("User sa id-jem " + userRatingInfo.getId().toString() + " ne postoji!");
---
+++ user-microservice/src/main/java/user/usermicroservice/grpc/EventSubmission.java
@@ -0,0 +1,46 @@
+package user.usermicroservice.grpc;
+
+import com.netflix.appinfo.InstanceInfo;
+import com.netflix.discovery.EurekaClient;
+import io.grpc.ManagedChannel;
+import io.grpc.ManagedChannelBuilder;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.Calendar;
+
+@Configuration
+public class EventSubmission {
+
+    @Qualifier("eurekaClient")
+    @Autowired
+    private EurekaClient eurekaClient;
+    public void addEvent(Events.ActionType tipAkcije, String nazivResursa) {
+
+        try {
+            InstanceInfo instanceInfo = eurekaClient.getNextServerFromEureka("system-events", false);
+            ManagedChannel channel= ManagedChannelBuilder.forAddress(instanceInfo.getIPAddr(), instanceInfo.getPort()).usePlaintext().build();
+
+            actionGrpc.actionBlockingStub stub=actionGrpc.newBlockingStub(channel);
+            Calendar c=Calendar.getInstance();
+            String ts=c.getTime().toString();
+
+            Events.Response response=stub.logAction(Events.Request.newBuilder()
+                    .setTimestamp(ts)
+                    .setNazivServisa("user-service")
+                    .setIdKorisnik(10000L)
+                    .setTipAkcije(tipAkcije)
+                    .setNazivResursa(nazivResursa)
+                    .build()
+            );
+            System.out.println(response.getResponseTypeValue());
+            System.out.println(response.getResponseContent());
+            channel.shutdown();
+        }
+        catch(Exception e) {
+            System.out.println("Greska u grpc komunikaciji!");
+        }
+
+    }
+}
